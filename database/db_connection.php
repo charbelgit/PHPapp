3ab8eaa067b4a69c995cf1cfeb9953c7ff6b0fca
charbelgit <67757299+charbelgit@users.noreply.github.com>
2020-07-03 17:03:56 +0300

    Add files via upload

---
+++ database/db_connection.php
@@ -0,0 +1,8 @@
+<?php
+$conn = new mysqli("localhost" , "root", "" , "dbName");
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}else{
+		//echo "succeed";
+	}
+?>
